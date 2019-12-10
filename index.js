f945ce2ca58baf11e6d9252f8b1c76e95ba94d11
Foglio1024 <foglio1024@gmail.com>
2019-12-10 17:06:56 +0100

    add def

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
+const path = require('path'); 
 class Heartwood
 {
     constructor(mod)
     {
+        if (mod.region !== 'eu')
+        {
+            mod.log('This mod only supports EU version of the game.');
+            return;
+        }
+        mod.dispatch.addDefinition('S_COMPLETED_MISSION_INFO', 1, path.join(mod.rootFolder, 'S_COMPLETED_MISSION_INFO.1.def'));
+        mod.dispatch.addOpcode('S_COMPLETED_MISSION_INFO', 43612);
         const exclusions = [47957, 50601, 50602, 50603];
         mod.hook('S_COMPLETED_MISSION_INFO', 1, ev =>
         {
             let abort = false;
             // check that all the quests to exclude are already completed
-            exclusions.forEach(ex => {
-                if(ev.quests.findIndex(ex) === -1) 
+            exclusions.forEach(ex =>
+            {
+                if (ev.quests.findIndex(q => q.questId === ex) === -1)
                     abort = true;
             });
 
             // abort if any of them is missing, to avoid quest progression issues
-            if(abort)
+            if (abort)
                 return true;
 
             // build new array with only the quests which are not in exclusions
             const newQuests = [];
             ev.quests.forEach(q =>
             {
-                if (exclusions.includes(q.questId)) 
+                if (exclusions.includes(q.questId))
                     return;
                 newQuests.push(q);
             });
