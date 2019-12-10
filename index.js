c5ab2c9e428bd5aa7bc4e6378598e43a864f2fd0
Foglio1024 <foglio1024@gmail.com>
2019-12-10 18:52:43 +0100

    enough

--- index.js
+++ index.js
@@ -6,59 +6,72 @@ class Heartwood
     constructor(mod)
     {
         this.mod = mod;
-
+        
+        // are there other regions outside EU?
         if (mod.region !== 'eu')
         {
             mod.log('This mod only supports EU version of the game.');
             return;
         }
 
+        // add def for p88 until it's added to tera-data
         mod.dispatch.addDefinition('S_COMPLETED_MISSION_INFO', 1, path.join(mod.rootFolder, 'S_COMPLETED_MISSION_INFO.1.def'));
         mod.dispatch.addOpcode('S_COMPLETED_MISSION_INFO', 43612);
 
+        // fuck JS man
+        this.onCompletedMissionInfo = function(ev)
+        {
+            if (!mod.settings.enabled) return true;
+            let abort = false;
+            // check that all the quests to exclude are already completed
+            exclusions.forEach(ex =>
+            {
+                if (ev.quests.findIndex(q => q.questId === ex) === -1)
+                    abort = true;
+            });
+    
+            // abort if any of them is missing, to avoid quest progression issues
+            if (abort)
+                return true;
+    
+            // build new array with only the quests which are not in exclusions
+            const newQuests = [];
+            ev.quests.forEach(q =>
+            {
+                if (exclusions.includes(q.questId))
+                    return;
+                newQuests.push(q);
+            });
+    
+            // send the modified array to the client
+            mod.send('S_COMPLETED_MISSION_INFO', 1, {
+                quests: newQuests
+            });
+    
+            // block full list
+            return false;
+        }
+
+        // check status
+        this.onEnabledChanged();
+
+        // add toggle command
         mod.command.add('heartwood', () =>
         {
             mod.settings.enabled = !mod.settings.enabled;
             mod.command.message(`<font color="#cccccc">Mod </font><font color="#${(mod.settings.enabled ? '42F5AD' : 'F05164')}">${(mod.settings.enabled ? 'en' : 'dis')}abled</font><font color="#cccccc">. Relog to character selection and back to apply the change.</font>`);
-            if (mod.settings.enabled)
-                mod.hook('S_COMPLETED_MISSION_INFO', 1, this.onCompletedMissionInfo);
-            else 
-                mod.unhook('S_COMPLETED_MISSION_INFO', 1, this.onCompletedMissionInfo);
+            this.onEnabledChanged();
         });
-
     }
 
-    onCompletedMissionInfo = function(ev){
-        let abort = false;
-        // check that all the quests to exclude are already completed
-        exclusions.forEach(ex =>
-        {
-            if (ev.quests.findIndex(q => q.questId === ex) === -1)
-                abort = true;
-        });
-
-        // abort if any of them is missing, to avoid quest progression issues
-        if (abort)
-            return true;
-
-        // build new array with only the quests which are not in exclusions
-        const newQuests = [];
-        ev.quests.forEach(q =>
-        {
-            if (exclusions.includes(q.questId))
-                return;
-            newQuests.push(q);
-        });
-
-        // send the modified array to the client
-        this.mod.send('S_COMPLETED_MISSION_INFO', 1, {
-            quests: newQuests
-        });
-
-        // block full list
-        return false;
-
+    onEnabledChanged()
+    {
+        if (this.mod.settings.enabled)
+            this.mod.hook('S_COMPLETED_MISSION_INFO', 1, this.onCompletedMissionInfo);
+        else
+            this.mod.unhook('S_COMPLETED_MISSION_INFO', 1, this.onCompletedMissionInfo);
     }
+
 }
 
 module.exports = Heartwood;
